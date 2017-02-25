998584c6a3d00a210e78178ad3097eb43e600df1
Ritwik Bhardwaj <ritwikbhardwaj098@gmail.com>
2017-02-25 13:15:30 -0500

    config updated

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   amadeusKey: 'DGvLOHEzcoQDkdxau23wuaZfJtdKYhsz',
-  googleMapsKey: 'AIzaSyBEh4Tpsk7rjTHwz6Jcynv8kzIaOy3VGSU'
+  googleMapsKey: 'AIzaSyBEh4Tpsk7rjTHwz6Jcynv8kzIaOy3VGSU',
+  sitaAirportKey: '45fbe9a28436552f7427d7066b5cb4e4'
 }
