abf2935ae96e7f639c02a4e90d1512a0122eb0b7
Ritwik Bhardwaj <ritwikbhardwaj098@gmail.com>
2017-02-25 14:52:41 -0500

    /sendTravelInformation route done

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	/*var body = {
- 		location: 'ORD',
+	/*
+	var body = {
+ 		location: 'WAS',
  		check_in : '2017-03-15',
  		check_out : '2017-03-19',
- 		radius : '42',
- 		number_of_results : '10'
+ 		radius : '50',
+ 		number_of_results : '3',
+ 		show_sold_out : true
  	};
     amadeus.hotelSearch(body,(err,data)=>{
   		console.log(data);
@@ -21,18 +23,23 @@ router.get('/', function(req, res, next) {
     	console.log(body);
     });
 	*/
-	var responseJSON = {};
+	
+	
+});
+
+router.get('/sendTravelInformation',function(req,res,next){
+	var finalListOfPlaces = [];
 	var prop = {
 		origin : "BOS",
 		departure : "2017-03-15",
 		duration : 4,
-		max_price : 350
+		max_price : 450
 	}
 	var totalBudget = prop.max_price;
 	prop.max_price -= prop.duration*51; 
 	amadeus.flightSearch(prop,(err,data)=>{
 
-		console.log(data.origin);
+		//console.log(data.origin);
 		data.results.forEach(function(value,i,array){
 			var hotelBody = {
 				location : data.results[i].destination,
@@ -41,18 +48,15 @@ router.get('/', function(req, res, next) {
 				radius : '42',
 				number_of_results : '5'
 			};
-			amadeus.hotelSearch(hotelBody, (err,hotelRes)=>{
-				if(hotelRes.results[0] != undefined){
-					var minHotelCost = hotelRes.results[0].total_price.amount;
-					if( 1.1*(minHotelCost + (18*prop.duration)) > (prop.max_price - data.results[i].price + 51*prop.duration)){
-						responseJSON.destinations = [];
-						console.log(data.results[i]);
-					}
-				}
-			});
+			
+			var minHotelCost = 80*prop.duration;
+			if( (minHotelCost + (18*prop.duration))+ (prop.duration*(minHotelCost + (18*prop.duration))/100)> (prop.max_price - data.results[i].price + 96*prop.duration)){
+				finalListOfPlaces.push(data.results[i]);
+				console.log(data.results[i]);
+			}
+			
 		});
 	});
-
 });
 
 module.exports = router;
