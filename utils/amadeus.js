d99e2df5334dd31cf0b86f4d0777fd461c70bd2d
Ritwik Bhardwaj <ritwikbhardwaj098@gmail.com>
2017-02-25 11:37:18 -0500

    more work in amadeus and google maps

--- utils/amadeus.js
+++ utils/amadeus.js
@@ -11,8 +11,8 @@ function flightSearch(properties, cb) {
   var url = 'https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search';
   properties.apikey = amadeusKey;
   request({ url: url, qs: properties }, function(err, response, body) {
-    var b = JSON.parse(body);
-    cb(err, b);
+
+    cb(err, body);
   });
 }
 
@@ -22,9 +22,9 @@ function hotelSearch(properties, cb){
  		properties.apikey = amadeusKey;
  		var url = 'https://api.sandbox.amadeus.com/v1.2/hotels/search-airport'
  		request({ url: url, qs: properties }, function(err, response, body) {
-		    var b = JSON.parse(body);
+		    
 		    console.log(body);
-		    cb(err,b);
+		    cb(err,body);
 		});
 }
 
