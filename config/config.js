1db8b84e1f37837c61e5792b3e2073ed1efaf925
Jonathan Xu <jonathanj.xu@yahoo.com>
2017-02-25 15:08:01 -0600

    Yelp config keys

--- config/config.js
+++ config/config.js
@@ -4,4 +4,10 @@ module.exports = {
   sitaAirportKey: '45fbe9a28436552f7427d7066b5cb4e4',
   googlePlacesKey: 'AIzaSyBEh4Tpsk7rjTHwz6Jcynv8kzIaOy3VGSU',
   mongoConnection: 'mongodb://dbadmin:dbpassword@ds117859.mlab.com:17859/boilermake'
+  yelp: {
+    consumerKey: 'r9Ekvr7oS4fi1l6Nmqtx6w',
+    consumerSecret: 'KSgT5lmnkV22iWWhSesiGQZ6Btg',
+    token: '57N4ip2dhyWEemGrKpFJysFv6djedJk3',
+    tokenSecret: 'Xnc6OyuVhyfghDcpErA9xQSC--c'
+  }
 }
