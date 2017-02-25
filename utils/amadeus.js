189412c3eb1f7112ad947f03a9346e61a3e89651
Ritwik Bhardwaj <ritwikbhardwaj098@gmail.com>
2017-02-25 13:11:48 -0500

    progress on amadeus

--- utils/amadeus.js
+++ utils/amadeus.js
@@ -11,8 +11,8 @@ function flightSearch(properties, cb) {
   var url = 'https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search';
   properties.apikey = amadeusKey;
   request({ url: url, qs: properties }, function(err, response, body) {
-
-    cb(err, body);
+  	var b = JSON.parse(body);
+    cb(err, b);
   });
 }
 
@@ -22,9 +22,9 @@ function hotelSearch(properties, cb){
  		properties.apikey = amadeusKey;
  		var url = 'https://api.sandbox.amadeus.com/v1.2/hotels/search-airport'
  		request({ url: url, qs: properties }, function(err, response, body) {
-		    
-		    console.log(body);
-		    cb(err,body);
+		    var b = JSON.parse(body);
+		    console.log(b);
+		    cb(err,b);
 		});
 }
 
