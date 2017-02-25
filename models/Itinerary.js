969dc175c5b7fda35536f0926e561694307902d7
Jonathan Xu <jonathanj.xu@yahoo.com>
2017-02-25 12:13:31 -0600

    Itinerary subdocument (event)

--- models/Itinerary.js
+++ models/Itinerary.js
@@ -1,12 +1,22 @@
 var mongoose = require('mongoose');
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
+var EventSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  cost: { type: Number, default: 0 },
+  day: { type: Number, required: true },
+  startTime: { type: Number, required: true },
+  endTime: { type: Number, required: true },
+  imageUrl: { type: String }
+});
+
 var ItinerarySchema = new mongoose.Schema({
   title: { type: String, required: true },
-  schedule: [{ type: Object }],
+  schedule: [EventSchema],
   days: { type: Number },
-  city: { type: String, required: true },
-  userId: { type: ObjectId }
+  city: { type: String, required: true, index: true },
+  userId: { type: ObjectId },
+  rating: { type: Number }
 },{
   minimize: false,
   timestamps: true
--- routes/api/itinerary.js
+++ routes/api/itinerary.js
@@ -21,4 +21,39 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/**
+ * @api {get} /api/itinerary/city/:city Get Itineraries By City
+ * @apiName GetCityItineraries
+ * @apiGroup Itinerary
+ * @apiDescription This path gets a list of all itineraries for a specific city
+ * @apiSuccessExample Success-Response
+ * [...]
+*/
+router.get('/city/:city', function(req, res, next) {
+  Itinerary.find({ city: req.params.city }, function(err, doc) {
+    if (err)
+      return next(err);
+    res.json(doc);
+  });
+});
+
+/**
+ * @api {post} /api/itinerary Create Itineraries
+ * @apiName CreateItineraries
+ * @apiGroup Itinerary
+ * @apiDescription This path allows creation of itineraries
+ * @apiSuccessExample Success-Response
+ * [...]
+*/
+router.post('/', function(req, res, next) {
+  Itinerary.create(function(err, doc) {
+    if (err)
+      return next(err);
+    res.json({
+      success: true,
+      itinerary: doc
+    });
+  });
+});
+
 module.exports = router;
