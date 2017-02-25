c45719bd821c5bf1e847199498187ef064c1dadd
Jonathan Xu <jonathanj.xu@yahoo.com>
2017-02-25 14:00:50 -0600

    Fix way index is being sent, remove ejs

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
-var amadeus = require('../utils/amadeus.js');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html');
-  amadeus.hotelSearch({},(err,data)=>{
-  	console.log(data);
-  });
+  res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
 module.exports = router;
