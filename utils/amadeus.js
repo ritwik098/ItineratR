76963e5c1bb03d88043ef57d041f8768c0a94ef5
Jonathan Xu <jonathanj.xu@yahoo.com>
2017-02-25 01:16:04 -0600

    Config file

---
+++ utils/amadeus.js
@@ -0,0 +1,13 @@
+var request = require('request');
+
+var amadeusKey = require('../config/config.js').amadeusKey;
+
+function flightSearch(properties) {
+  var url = 'https://api.sandbox.amadeus.com/v1.2/flights/inspiration-search';
+  //request({url: url, })
+  return {}
+}
+
+module.exports = {
+
+}
