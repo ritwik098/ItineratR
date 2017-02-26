8ff4e92b20e33c9ec0ec05c543160e18cbf4d7fa
Jonathan Xu <jonathanj.xu@yahoo.com>
2017-02-26 08:55:25 -0600

    Minor bug catch in scheduling

--- test/schedule.js
+++ test/schedule.js
@@ -9,7 +9,7 @@ describe('Scheduler', function() {
     var now = new Date();
     var then = new Date();
     then.setDate(then.getDate() + 3);
-    schedule(now, then, 'New York City', function(err, body) {
+    schedule(now, then, 'Boston', function(err, body) {
       console.log(err);
       console.log(body);
       body.should.be.a('array');
